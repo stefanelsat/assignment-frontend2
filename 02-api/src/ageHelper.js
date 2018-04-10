a83a5a70358b2f4c8c431f467fe00bc2090e9f2c
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-04-10 09:36:20 +0200

    remove 02-api from this repo

--- 02-api/src/ageHelper.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  Handlebars.registerHelper("age", function(birthdate) {
-    let today = new Date()
-    let birthday = new Date(birthdate)
-    let inYears = today.getFullYear() - birthday.getFullYear()
-    return inYears.toString()
-  })
-}
